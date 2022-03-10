29872c7a7c2515d86af510f5673f7b0d1ab31924
benja <benjioe14@gmail.com>
2022-03-10 23:55:14 +0100

    conditions

---
+++ epreuves/4.conditions.test.js
@@ -0,0 +1,72 @@
+// let assert = require("assert");
+
+// const REPONSE = "";
+
+// // En JavaScript, un objet correspond à ce que l'on appelle Dictionnaire ou Hashmap dans d'autres langages
+
+// describe("conditions", function () {
+//   it("le if permet d'appliquer du code que si la condition est vrai", function () {
+//     let resultat = 0;
+
+//     if (resultat === 0) {
+//       resultat = 3;
+//     }
+
+//     if (resultat === 5) {
+//       resultat = 10;
+//     }
+
+//     if (resultat === 0) {
+//       resultat = 42;
+//     }
+
+//     assert.equal(resultat, REPONSE);
+//   });
+
+//   it("si aucune condition n'est vrais, c'est le else qui s'applique", function () {
+//     let resultat = 0;
+
+//     if (resultat === 42) {
+//       resultat = 12;
+//     } else {
+//       resultat = 21;
+//     }
+//     assert.equal(resultat, REPONSE);
+//   });
+
+//   it("le ternaire est un if/else compressé", function () {
+//     let resultat = 0;
+
+//     resultat = resultat === 42 ? 12 : 21;
+//     assert.equal(resultat, REPONSE);
+//   });
+
+//   it("le elseif si on doit tester plusieurs conditions", function () {
+//     let resultat = 0;
+
+//     if (resultat === 42) {
+//       resultat = 12;
+//     } else if (resultat === 0) {
+//       resultat = 3;
+//     } else {
+//       resultat = 21;
+//     }
+//     assert.equal(resultat, REPONSE);
+//   });
+
+//   it("le switch case permet aussi de tester plusieurs conditions", function () {
+//     let resultat = 0;
+
+//     switch (resultat) {
+//       case 42:
+//         resultat = 12;
+//         break;
+//       case 0:
+//         resultat = 11;
+//         break;
+//       default:
+//         resultat = 21;
+//     }
+//     assert.equal(resultat, REPONSE);
+//   });
+// });
