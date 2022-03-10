3a022df7ac27237b720d57f48a0e549455ff4984
benja <benjioe14@gmail.com>
2022-03-11 00:03:21 +0100

    boucles

---
+++ epreuves/5.boucles.test.js
@@ -0,0 +1,37 @@
+// let assert = require("assert");
+
+// const REPONSE = "";
+
+// describe("boucles", function () {
+//   it("le for permet de créer une boucle", function () {
+//     let resultat = 0;
+
+//     for (let i = 0; i < 10; i++) {
+//         resultat = resultat + 5;
+//     }
+
+//     assert.equal(resultat, REPONSE);
+//   });
+
+//   it("le for of facilite le parcours de tableaux", function () {
+//     const tableau = [1,2,3,4];
+//     let resultat = 0;
+
+//     for (let element of tableau) {
+//         resultat = resultat + element;
+//      }
+//     assert.equal(resultat, REPONSE);
+//   });
+
+
+//   it("les objets aussi peuvent être parcourus", function () {
+//     const objet = {"A": 1, "B": 2};
+//     let resultat = 0;
+
+//     for (let key in objet) {
+//         resultat = resultat + objet[key];
+//      }
+//     assert.equal(resultat, REPONSE);
+//   });
+
+// });
