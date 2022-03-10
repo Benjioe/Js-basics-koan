fc0cc4602274571400aba7783f921a00dae244e3
Jeremie S <3914641+jsz4n@users.noreply.github.com>
2022-03-11 00:33:12 +0100

    fix typos

--- epreuves/2.tableau.test.js
+++ epreuves/2.tableau.test.js
@@ -11,38 +11,38 @@
 //       assert.equal(multiTypeArray[0], REPONSE);
 //       assert.equal(multiTypeArray[1], REPONSE);
 //       assert.equal(multiTypeArray[2], REPONSE);
-//       assert.equal(multiTypeArray[3], REPONSE);
+//       assert.deepEqual(multiTypeArray[3], REPONSE);
 //     });
   
 //     it("y ajouter des éléments", function () {
 //       let tableau = [1, 2, 3, 4];
   
 //       assert.equal(tableau.length, REPONSE);
-//       fourNumberArray.push(5, 6);
+//       tableau.push(5, 6);
 //       assert.equal(tableau.length, REPONSE);
 //     });
   
 //     it("supprimer des valeurs", function () {
 //       let array = ["peanut", "butter", "and", "jelly"];
   
-//       assert.equal(array.slice(0, 1), REPONSE);
-//       assert.equal(array.slice(0, 2), REPONSE);
-//       assert.equal(array.slice(2, 2), REPONSE);
-//       assert.equal(array.slice(2, 20), REPONSE);
-//       assert.equal(array.slice(3, 0), REPONSE);
-//       assert.equal(array.slice(3, 100), REPONSE);
-//       assert.equal(array.slice(5, 1), REPONSE);
+//       assert.deepEqual(array.slice(0, 1), REPONSE);
+//       assert.deepEqual(array.slice(0, 2), REPONSE);
+//       assert.deepEqual(array.slice(2, 2), REPONSE);
+//       assert.deepEqual(array.slice(2, 20), REPONSE);
+//       assert.deepEqual(array.slice(3, 0), REPONSE);
+//       assert.deepEqual(array.slice(3, 100), REPONSE);
+//       assert.deepEqual(array.slice(5, 1), REPONSE);
 //     });
   
 //     it("ou les retirer", function () {
 //       let array = [1, 2, 3];
 //       let poppedValue = array.pop();
 //       assert.equal(poppedValue, REPONSE);
-//       assert.equal(array, REPONSE);
+//       assert.deepEqual(array, REPONSE);
 
 //       let shiftedValue = array.shift();
 //       assert.equal(shiftedValue, REPONSE);
-//       assert.equal(array, REPONSE);
+//       assert.deepEqual(array, REPONSE);
 //     });
   
 //   });
